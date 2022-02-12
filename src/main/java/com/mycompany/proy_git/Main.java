34aaedb1e28ca4f99660bb41c65755049a47d3ed
Jake82181 <jakelin_bedoya82181@elpoli.edu.co>
2022-02-12 09:34:57 -0500

    Probando mi primer repositorio

---
+++ src/main/java/com/mycompany/proy_git/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proy_git;
+
+/**
+ *
+ * @author yk122
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        // TODO code application logic here
+    }
+    
+}
